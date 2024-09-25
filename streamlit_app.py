4de9b6fa7f572c66b2f76f306b2fe6836fc418b8
Saad <84753254+SaadShanghai244@users.noreply.github.com>
2024-09-26 02:28:11 +0500

    Update streamlit_app.py
    Accessing from server

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,39 +1,25 @@
-# streamlit_app.py (Streamlit Application with local transcription)
+# streamlit_app.py (Streamlit Application)
 import streamlit as st
-import whisper
-import os
-from tempfile import NamedTemporaryFile
+import requests
 
 st.title("Audio to Text Transcription App")
-
-# Load Whisper model once for the Streamlit app
-model = whisper.load_model("small", device="cpu")
 
 # Allow the user to upload an audio file (MP3, WAV)
 uploaded_file = st.file_uploader("Choose an audio file...", type=["mp3", "wav"])
 
 if uploaded_file is not None:
-    # Display the uploaded file name and play it
+    # Display the uploaded file name
     st.audio(uploaded_file, format='audio/mp3')
     st.write(f"Uploaded file: {uploaded_file.name}")
 
-    # Transcribe the audio locally when the button is clicked
+    # Call the FastAPI service
     if st.button('Transcribe'):
-        # Save the uploaded file to a temporary location
-        with NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio_file:
-            temp_audio_file.write(uploaded_file.read())
-            temp_audio_file_path = temp_audio_file.name
+        files = {'file': uploaded_file.getvalue()}
+        # Ensure the FastAPI app is running at this address
+        response = requests.post("http://44.211.66.81/:8000/transcribe/", files=files)
+        result = response.json()
 
-        try:
-            # Transcribe the audio using Whisper
-            result = model.transcribe(temp_audio_file_path)
-            transcription = result['text']
-
-            # Display the transcription
-            st.write(f"Transcription: {transcription}")
-
-            # Delete the temporary file after transcription
-            os.remove(temp_audio_file_path)
-
-        except Exception as e:
-            st.write(f"Error: {str(e)}")
+        if response.status_code == 200:
+            st.write(f"Transcription: {result['transcription']}")
+        else:
+            st.write(f"Error: {result.get('error', 'Unknown error occurred')}")
